8a3f82faf025b82c8579d87789f17ec0fb9e7d99
Leonardo Alves <leolorena.alves@gmail.com>
2024-09-09 08:56:08 -0300

    DEBORA

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,31 +1,54 @@
 
 import './index.scss';
-import { useEffect } from  'react'
+import { useEffect } from 'react'
 
-export default function Card() {
+export default function Card(Props) {
   let corPrimaria = '#A3E5BA'
   let corSegundaria = '#EFFBE2'
-  
+
   let data = true
 
-  
+
+  switch (Props.status) {
+    case 'Completo':
+      corPrimaria = '#A3E5BA'
+      corSegundaria = '#EFFBE2'
+      break;
+    case 'Em andamento':
+      corPrimaria = '#DCA3E5'
+      corSegundaria = '#EDE2FB'
+      break;
+    case 'Criado':
+      corPrimaria = '#C3C3C3'
+      corSegundaria = '#F2F2F2'
+      break;
+    case 'Evento':
+      corPrimaria = '#F6E448'
+      corSegundaria = '#FAF4C1'
+      break;
+    default:
+      corPrimaria = '#EDF2F4'
+      corSegundaria = '#313131'
+      break;
+  }
+
   return (
     <div className='comp-card'>
       <div className='cards'>
         <div className='card' style={{ backgroundColor: corSegundaria }}>
           <div className='card-cabecalho' style={{ backgroundColor: corPrimaria }}>
-            <h2>Componentes</h2>
+            <h2>{Props.titulo}</h2>
           </div>
           <div className='card-conteudo'>
             <h3>Assuntos</h3>
             <pre>
-              - Objetos e Listas de Objetos<br />
-              - Componentes
+              {Props.assunto}
             </pre>
-    {data && <p className='tag'>26/agosto</p>}
-        
+            {data && <p className='tag'>{Props.data}</p>}
+            {Props.trabalho && <p className='tag'>Entrega Trab.</p>}
+
           </div>
-        
+
         </div>
       </div>
     </div>
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,10 +3,38 @@ import Cabecalho from '../components/cabecalho/index'
 import Card from '../components/card';
 
 export default function App() {
+
+  let corPrimaria = ''
+  let corSegundaria = ''
+  let card = [
+    {
+      "titulo": "Componentes",
+      "assunto": "- Objetos e Listas de Objetos",
+      "data": "26/Agosto",
+      "trabalho": false,
+      "status": "Completo"
+    },
+    {
+      "titulo": "rato",
+      "assunto": "tchufo",
+      "data": "2/agosto",
+      "trabalho": true,
+      "status": "Em andamento"
+    },
+  ]
+
   return (
     <div className="App">
       <Cabecalho />
-      <Card/>
+      {card.map(item => (
+        <Card
+          titulo={item.titulo}
+          assunto={item.assunto}
+          data={item.data}
+          trabalho={item.trabalho}
+          status={item.status}
+        />
+      ))}
     </div>
   );
 }
